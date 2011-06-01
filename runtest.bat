09bdf06658c2607580d71e46dd488a43af5a1777
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-05-31 19:41:11 -0700

    rename test.go => upskirt_ref_test.go

--- runtest.bat
+++ runtest.bat
@@ -3,10 +3,10 @@
 8g -o bin\markup.8 html.go markup.go
 @if ERRORLEVEL 1 EXIT /B 1
 
-8g -o bin\test.8 -I bin test.go 
+8g -o bin\upskirt_ref_test.8 -I bin upskirt_ref_test.go 
 @if ERRORLEVEL 1 EXIT /B 1
 
-8l -o bin\markuptest.exe -L bin bin\test.8
+8l -o bin\upskirtreftest.exe -L bin bin\upskirt_ref_test.8
 @if ERRORLEVEL 1 EXIT /B 1
 
-bin\markuptest.exe
+bin\upskirtreftest.exe
